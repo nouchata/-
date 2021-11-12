a351876de85ce658ba5be21bc3b853bf8a5e6e01
tmatis <tmatis@student.42.fr>
2021-11-12 18:35:51 +0100

    fix warn

--- requirements/front-end/src/App.tsx
+++ requirements/front-end/src/App.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<div className="app">
+			Hello world
+		</div>
+	)
 }
 
 export default App;
