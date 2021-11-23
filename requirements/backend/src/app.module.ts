b81bac2dcdf8f2390d4faa4fa7c2eabf278eddfd
tmatis <tmatis@student.42.fr>
2021-11-23 13:57:05 +0100

    backend config

--- requirements/backend/src/app.module.ts
+++ requirements/backend/src/app.module.ts
@@ -1,9 +1,29 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+let dyn_import = []
+
+if (process.env.RUN_ENV !== "PROD") {
+	Logger.log('Running in DEV mode', 'Config');
+	dyn_import.push(ConfigModule.forRoot({
+		envFilePath: 'dev.env',
+		isGlobal: true
+	  }));
+}
+else
+{
+	Logger.log('Running in PROD mode', 'Config');
+	dyn_import.push(ConfigModule.forRoot({
+		isGlobal: true
+	  }));
+}
+
+
+
 @Module({
-  imports: [],
+  imports: dyn_import,
   controllers: [AppController],
   providers: [AppService],
 })
