53c84af60b802066af7ad1ccccdb17bee1330f2c
nouchata <nouchata@student.42.fr>
2021-11-29 20:51:02 +0100

    i'm tired

--- requirements/frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-// test('renders learn react link', () => {
-//   render(<App />);
-  // const title = screen.getByText(/ft_transcendence/i);
-  // expect(title).toBeInTheDocument();
-// });
