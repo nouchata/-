e41902f41b1fc519f6d5b944c073d0701883fb51
usr99 <mael.91150@hotmail.fr>
2021-11-30 18:52:35 +0100

    Better user entity * new fields to fit the subject requirements

--- requirements/backend/src/user/entities/user.entity.ts
+++ requirements/backend/src/user/entities/user.entity.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
-import { UserInterface, UserRole } from "../interface/UserInterface";
+import { UserInterface, UserRole, UserState } from "../interface/UserInterface";
 
 @Entity({ name: 'users' })
 export class User implements UserInterface {
@@ -27,6 +27,15 @@ export class User implements UserInterface {
 	@Column()
 	@ApiProperty(
 		{
+			description: "The path to the avatar of the user",
+			example: "avatar_16.jpg",
+		}
+	)
+	avatar: string;
+
+	@Column()
+	@ApiProperty(
+		{
 			enum: ['standard', 'moderator', 'admin'],
 			description: "The role of the user",
 			example: "standard",
@@ -34,7 +43,6 @@ export class User implements UserInterface {
 	)
 	role: UserRole;
 
-	
 	@CreateDateColumn(
 		{
 			update: false
@@ -48,6 +56,50 @@ export class User implements UserInterface {
 	)
 	createdAt: Date;
 
+	@Column()
+	@ApiProperty(
+		{
+			description: "The user's rank in the ladder",
+			exemple: 598,
+		}
+	)
+	rank: number;
+
+	@Column("simple-array")
+	@ApiProperty(
+		{
+			description: "The count of victories/losses of the user",
+			exemple: [22, 13],
+		}
+	)
+	vdRatio: [number, number];
+
+	@Column("simple-array")
+	@ApiProperty(
+		{
+			description: "The history of the previous matches of the user",
+			exemple: [{ players: [56, 22], score: [5, 3], duration: 300 }],
+		}
+	)
+	history: UserMatchHistory[];
+
+	@Column("simple-array")
+	@ApiProperty(
+		{
+			description: "The user's friends list (only contains user's id)",
+			exemple: [18, 12, 27],
+		}
+	)
+	friends: number[];
+
+	@Column()
+	@ApiProperty(
+		{
+			description: "The state of the user",
+			exemple: "online",
+		}
+	)
+	state: UserState;
 
 	hasRole(role: UserRole): boolean {
 		console.log(role);
--- requirements/backend/src/user/interface/UserInterface.ts
+++ requirements/backend/src/user/interface/UserInterface.ts
@@ -1,5 +1,13 @@
 export type UserRole = 'standard' | 'moderator' | 'admin';
 
+export type UserState = 'online' | 'offline' | 'ingame';
+
 export interface UserInterface {
 	login: string;	
+}
+
+export interface UserMatchHistory {
+	players: [number, number];	// winner / loser
+	score: [number, number];	// winner / loser
+	duration: number;			// in seconds
 }
