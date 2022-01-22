63d65ce217efb6e6f459f332ae33c7c3a01acfb2
tmatis <tmatis@student.42.fr>
2022-01-22 15:20:48 +0000

    format

--- requirements/frontend/src/components/notification/Notifications.tsx
+++ requirements/frontend/src/components/notification/Notifications.tsx
@@ -12,42 +12,41 @@ const Notifications = () => {
 		<>
 			{
 				(notificationHandler?.numberOfNotifications as number > 0) && <NotificationCenter />
-
 			}
 			{
 				notificationHandler?.show_new_notification && new_notif &&
 				<div className='new-notification'>
 					<div key={new_notif.uuid} className='notification'>
-							{
-								new_notif.image &&
-								<img className='notification-image' src={new_notif.image} alt='notif-pic' />
-							}
-							<div className="notification-content">
-								<div className='notification-title'>{new_notif.name}</div>
-								<div className='notification-text'>{new_notif.content}</div>
-							</div>
-							{
-								new_notif.openAction &&
-								<div className='notification-action'>
-									<div
-										className='notification-open-action'
-										onClick={() => {
-											new_notif.openAction && new_notif.openAction();
-											notificationHandler?.removeNotificationContext('chat');
-										}}
-									>
-										<div className='notification-open-action-text'>Open</div>
-									</div>
-								</div>
-							}
-							<img
-								className='notification-close'
-								src={close}
-								alt='close'
-								onClick={() => notificationHandler.removeNotification(new_notif.uuid)}
-							/>
-
+						{
+							new_notif.image &&
+							<img className='notification-image' src={new_notif.image} alt='notif-pic' />
+						}
+						<div className="notification-content">
+							<div className='notification-title'>{new_notif.name}</div>
+							<div className='notification-text'>{new_notif.content}</div>
 						</div>
+						{
+							new_notif.openAction &&
+							<div className='notification-action'>
+								<div
+									className='notification-open-action'
+									onClick={() => {
+										new_notif.openAction && new_notif.openAction();
+										notificationHandler?.removeNotificationContext('chat');
+									}}
+								>
+									<div className='notification-open-action-text'>Open</div>
+								</div>
+							</div>
+						}
+						<img
+							className='notification-close'
+							src={close}
+							alt='close'
+							onClick={() => notificationHandler.removeNotification(new_notif.uuid)}
+						/>
+
+					</div>
 				</div>
 			}
 		</>
