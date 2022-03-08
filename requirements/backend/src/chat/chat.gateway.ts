e58d6813a9a22af37d0a387c16f4a9a5744b17b7
tmatis <tmatis@student.42.fr>
2022-03-08 14:53:54 +0100

    rework ws

--- requirements/backend/src/chat/chat.gateway.ts
+++ requirements/backend/src/chat/chat.gateway.ts
@@ -22,32 +22,49 @@ export class ChatGateway {
 	wsServer: Server;
 
 	// [userId: number] => Socket
-	private userSockets: { [userId: number]: Socket } = {};
+	private userSockets: {
+		[userId: number]: { socket: Socket; channels: number[] };
+	} = {};
 
 	async sendMessageToChannel(channelId: number, message: MessageDto) {
-		this.wsServer
-			.to('channel#' + channelId)
-			.emit('receiveMessage', { ...message, channelId: channelId });
+		Object.values(this.userSockets)
+			.filter(({ channels }) => channels.includes(channelId))
+			.forEach(({ socket }) => {
+				socket.emit('receiveMessage', {
+					...message,
+					channelId: channelId,
+				});
+			});
 	}
 
 	async addNewUser(channelId: number, user: User) {
-		this.wsServer
-			.to('channel#' + channelId)
-			.emit('newUser', { ...user, channelId: channelId });
+		Object.values(this.userSockets)
+			.filter(({ channels }) => channels.includes(channelId))
+			.forEach(({ socket }) => {
+				socket.emit('newUser', { ...user, channelId: channelId });
+			});
 	}
 
 	async removeUserChannel(channelId: number, user: User) {
 		// remove user from room
-		this.wsServer
-			.to('channel#' + channelId)
-			.emit('removeUser', { ...user, channelId: channelId });
-		this.userSockets[user.id].leave('channel#' + channelId);
+		Object.values(this.userSockets)
+			.filter(({ channels }) => channels.includes(channelId))
+			.forEach(({ socket }) => {
+				socket.emit('removeUser', { ...user, channelId: channelId });
+			});
+		if (!this.userSockets[user.id]) return;
+		this.userSockets[user.id].channels = this.userSockets[
+			user.id
+		].channels.filter((c) => c !== channelId);
 	}
 
 	// handle user connection
 	@UseGuards(WsGroupGuard)
 	handleConnection(client: Socket & { request: { user: User } }) {
-		this.userSockets[client.request.user.id] = client;
+		this.userSockets[client.request.user.id] = {
+			socket: client,
+			channels: [],
+		};
 	}
 
 	// handle user disconnection
@@ -66,7 +83,11 @@ export class ChatGateway {
 
 		if (channelFound) {
 			if (channelFound.canUserAccess(client.request.user)) {
-				client.join('channel#' + channelId);
+				if (!this.userSockets[client.request.user.id])
+					throw new WsException('User not found');
+				this.userSockets[client.request.user.id].channels.push(
+					channelId
+				);
 				client.emit('joinedChannel', channelFound);
 			} else
 				throw new WsException(
