b4a58bf61ed5e075e4eaf299f0452cb0750ff3da
tmatis <tmatis@student.42.fr>
2022-03-14 13:08:41 +0100

    create punishement dto

---
+++ requirements/backend/src/chat/dtos/create-punishment.dto.ts
@@ -0,0 +1,41 @@
+import { PunishmentType } from '../entities/punishment.entity';
+import { IsNumber, IsString, IsOptional, IsIn, IsDate } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreatePunishmentDto {
+	@IsNumber()
+	@ApiProperty({
+		description: 'the id of the channel',
+		example: 1,
+	})
+	userId: number;
+
+	@IsNumber()
+	@ApiProperty({
+		description: 'the id of the channel',
+		example: 1,
+	})
+	channelId: number;
+
+	@IsString()
+	@IsOptional()
+	@ApiProperty({
+		description: 'the reason of the punishment',
+		example: 'spamming',
+	})
+	reason?: string;
+
+	@IsIn(['ban', 'mute'])
+	@ApiProperty({
+		description: 'the type of the punishment',
+		example: 'ban',
+	})
+	type: PunishmentType;
+
+	@IsOptional()
+	@IsDate()
+	@ApiProperty({
+		description: 'the date of the punishment',
+	})
+	duration?: Date;
+}
