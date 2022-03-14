ec67c8cdb7b531286234cf77eedcbf59f318897a
tmatis <tmatis@student.42.fr>
2022-03-14 14:04:13 +0100

    splitting again

--- requirements/frontend/src/components/homepage/SocialField/ChatBox.tsx
+++ requirements/frontend/src/components/homepage/SocialField/ChatBox.tsx
@@ -1,65 +1,8 @@
-import { faClose, faMaximize, faMinimize } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import InputChat from '../../chat/InputChat';
 import MessageArea from '../../chat/MessageArea';
-import ChatOption from '../../chat/Options/ChatOption';
 import { ChatSocket } from '../../chat/utils/ChatSocket';
+import ChatControl from './ChatControl';
 import { ChatState } from './HSocialField';
-
-
-
-const ChatControl = ({
-	chatSocket,
-	selectChannelIndex,
-	chatStatus,
-	setChatStatus,
-}: {
-	chatSocket: ChatSocket;
-	selectChannelIndex: number;
-	chatStatus: ChatState;
-	setChatStatus: (chatStatus: ChatState) => void;
-}) => {
-	return (
-		<div className="hsf-chat-controls">
-			<h2>{chatSocket?.channels[selectChannelIndex].name}</h2>
-			<ChatOption channel={chatSocket.channels[selectChannelIndex]} />
-			{chatStatus.state === 'OPENED' ? (
-				<button
-					title="Minimize"
-					onClick={() =>
-						setChatStatus({
-							state: 'MINIMIZED',
-						})
-					}
-				>
-					<FontAwesomeIcon icon={faMinimize} className="icon-options" />
-				</button>
-			) : (
-				<button
-					title="Maximize"
-					onClick={() =>
-						setChatStatus({
-							state: 'OPENED',
-						})
-					}
-				>
-					<FontAwesomeIcon icon={faMaximize} className="icon-options" />
-				</button>
-			)}
-
-			<button
-				title="Close"
-				onClick={() =>
-					setChatStatus({
-						state: 'CLOSED',
-					})
-				}
-			>
-				<FontAwesomeIcon icon={faClose} className="icon-options" />
-			</button>
-		</div>
-	);
-};
 
 const chatToggleCSS = (cs: ChatState) => {
 	let ret: string = 'hsf-chat';
