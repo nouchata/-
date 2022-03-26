d4256ba6398fc76d8b36ddcfb930ebed3027b147
tmatis <tmatis@student.42.fr>
2022-03-26 20:56:45 +0100

    rework socket

--- containers/frontend/src/components/game/game/GameWS.ts
+++ containers/frontend/src/components/game/game/GameWS.ts
@@ -1,13 +1,11 @@
-import socketIOClient, { Socket } from "socket.io-client";
-import { GameAction } from "./types/GameAction";
-import { ResponseState } from "./types/ResponseState";
-
-
+import socketIOClient, { Socket } from 'socket.io-client';
+import { GameAction } from './types/GameAction';
+import { ResponseState } from './types/ResponseState';
 
 class GameWS {
-	socket : Socket;
-	instanceId : number;
-	error : string | undefined;
+	socket: Socket;
+	instanceId: number;
+	error: string | undefined;
 
 	constructor(
 		instanceId: number,
@@ -19,23 +17,32 @@ class GameWS {
 
 		this.socket = socketIOClient(
 			process.env.REACT_APP_BACKEND_ADDRESS + '/game',
-			{ withCredentials: true }
+			{
+				path: process.env.REACT_APP_BACKEND_ADDRESS + '/socket.io',
+				withCredentials: true,
+			}
 		);
 
 		this.socket.on('exception', errorCallback);
 
 		this.socket.on('stateUpdate', stateCallback);
 
-		this.socket.emit('joinGame', { instanceId: this.instanceId, forceSpectator: forceSpectator ? 1 : 0 });
+		this.socket.emit('joinGame', {
+			instanceId: this.instanceId,
+			forceSpectator: forceSpectator ? 1 : 0,
+		});
 	}
 
 	emit(gameAction: GameAction) {
-		this.socket.emit('gameActionDispatcher', { instanceId: this.instanceId, gameAction });
+		this.socket.emit('gameActionDispatcher', {
+			instanceId: this.instanceId,
+			gameAction,
+		});
 	}
 
 	destroy() {
 		this.socket.disconnect();
 	}
-};
+}
 
 export { GameWS };
